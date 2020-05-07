273e744e671e3e4f9b7c83d1292dccf0a6802d07
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-05-07 15:58:11 +0530

    Created header, footer and note components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import Note from "../src/components/Note";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Note />
+      <Footer />
     </div>
   );
 }

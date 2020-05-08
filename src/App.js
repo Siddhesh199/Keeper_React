c3f181bbf8d1a276272f16402eea2b518fd0ec51
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-05-08 13:39:35 +0530

    Mapping through notes array

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import React from "react";
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
 import Note from "../src/components/Note";
+import notes from "../src/notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
